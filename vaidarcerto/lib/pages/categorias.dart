768cf80230770d6d5316656cce0632adb87d9d9a
Isabela Manzano <44615503890@escola928.edu>
2024-11-05 17:15:33 -0300

    oi

--- vaidarcerto/lib/pages/categorias.dart
+++ vaidarcerto/lib/pages/categorias.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:projetostreaming/pages/home.dart';
+import 'package:projetostreaming/pages/home.dart'; 
 import 'package:projetostreaming/shared/style.dart';
+
+void main() {
+  runApp(Categorias());
+}
 
 class Categorias extends StatelessWidget {
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
+      home: CategoriasScreen(), 
     );
   }
 }
 
+class CategoriasScreen extends StatefulWidget {
+  @override
+  _CategoriasScreenState createState() => _CategoriasScreenState();
+}
 
+class _CategoriasScreenState extends State<CategoriasScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String? _selectedValue;
+  bool _isDropdownOpened = false;
+
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+      ),
+
+      // NAVBAR
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.blue,
+        selectedIndex: currentPageIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search, color: Colors.white),
+            label: 'Pesquisa',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history, color: Colors.white),
+            label: 'Histórico',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person, color: Colors.white),
+            label: 'Você',
+          ),
+        ],
+      ),
+    );
+  }
+}
